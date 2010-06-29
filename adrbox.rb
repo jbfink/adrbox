9c744de9d0de18e64da2086d8620484b3583ac71
John Fink <jbfink@zamenhof>
2010-06-29 18:28:37 -0400

    Beginnings of spock lizard logic.

--- adrbox.rb
+++ adrbox.rb
@@ -15,4 +15,19 @@ elsif userchoice == "S" and computerchoice == "R" then puts losemessage
 else puts "WTF?"
 end
 end
+def Adrbox.spocklizard(userchoice, computerchoice)
+losemessage = "You lose!"
+winmessage = "You win!"
+tiemessage = "You tie!"
+if userchoice == "R" and computerchoice == "P" then puts losemessage
+elsif userchoice == "R" and computerchoice == "S" then puts winmessage
+elsif userchoice == "R" and computerchoice == "R" then puts tiemessage
+elsif userchoice == "P" and computerchoice == "P" then puts tiemessage
+elsif userchoice == "P" and computerchoice == "S" then puts losemessage
+elsif userchoice == "P" and computerchoice == "R" then puts winmessage
+elsif userchoice == "S" and computerchoice == "P" then puts winmessage
+elsif userchoice == "S" and computerchoice == "S" then puts tiemessage
+elsif userchoice == "S" and computerchoice == "R" then puts losemessage
+end
+end
 end
