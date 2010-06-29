6f72371f46b47e40c2e31a688636770969a36b93
John Fink <jbfink@zamenhof>
2010-06-29 18:35:24 -0400

    Finished spock lizard logic.

--- adrbox.rb
+++ adrbox.rb
@@ -19,6 +19,7 @@ def Adrbox.spocklizard(userchoice, computerchoice)
 losemessage = "You lose!"
 winmessage = "You win!"
 tiemessage = "You tie!"
+#spock is O.  Maybe we should change that?
 if userchoice == "R" and computerchoice == "P" then puts losemessage
 elsif userchoice == "R" and computerchoice == "S" then puts winmessage
 elsif userchoice == "R" and computerchoice == "R" then puts tiemessage
@@ -28,6 +29,16 @@ elsif userchoice == "P" and computerchoice == "R" then puts winmessage
 elsif userchoice == "S" and computerchoice == "P" then puts winmessage
 elsif userchoice == "S" and computerchoice == "S" then puts tiemessage
 elsif userchoice == "S" and computerchoice == "R" then puts losemessage
+elsif userchoice == "O" and computerchoice == "O" then puts tiemessage
+elsif userchoice == "L" and computerchoice == "L" then puts tiemessage
+elsif userchoice == "O" and computerchoice == "R" then puts winmessage
+elsif userchoice == "O" and computerchoice == "P" then puts losemessage
+elsif userchoice == "O" and computerchoice == "S" then puts winmessage
+elsif userchoice == "O" and computerchoice == "L" then puts losemessage
+elsif userchoice == "L" and computerchoice == "R" then puts losemessage
+elsif userchoice == "L" and computerchoice == "P" then puts winmessage
+elsif userchoice == "L" and computerchoice == "S" then puts losemessage
+else puts "WTF?"
 end
 end
 end
